0214a5568b6fc402f613e9f50d271f71089c9a9f
ruwaa-mohamed <ruwaa.ibrahem@gmail.com>
2020-07-15 17:27:37 +0200

    splitting the downstream analysis of the res objects based on BC subtype

---
+++ scripts/idc.R
@@ -0,0 +1,113 @@
+### IDC
+################################################################################
+## Extracting dds.run.idc from dds.run
+dds.run.idc <- dds.run[,dds.run$group %in% c("IDC_Normal", "IDC_Tumor")]
+dds.run.idc
+table(dds.run.idc$group)
+dds.run.idc$group <- factor(dds.run.idc$group)
+table(dds.run.idc$group)
+################################################################################
+## Creating the results (RES) object from the original dds.run
+res.idc <- results(dds.run, contrast=c("group", "IDC_Normal", "IDC_Tumor"), alpha=0.05,lfcThreshold=1)
+summary(res.idc)
+
+## Distribution of the LFC and p-adjusted value
+summary(res.idc$log2FoldChange)
+hist(res.idc$log2FoldChange, main="Distribution of the Log2 fold change in the results of IDC", xlab="Log2 Fold Change")
+summary(res.idc$padj)
+hist(res.idc$padj, main="Distribution of the adjusted p-value in the results of IDC", xlab="Adjusted p-value")
+
+## complete cases only
+res.idc.complete <- res.idc[complete.cases(res.idc),]
+################################################################################
+## Converting the results to DF
+res.idc.df <- as.data.frame(res.idc)
+res.idc.df <- res.idc.df[order(res.idc.df$padj),]
+write.csv(res.idc.df, "saved_objects/IDC.results.sorted.csv")
+################################################################################
+## Extracting DEGs
+res.idc.df.degs <- res.idc.df[res.idc.df$padj<0.05 & abs(res.idc.df$log2FoldChange)>1,]
+write.csv(res.idc.df.degs, "saved_objects/idc-degs-p.05-LFC1.csv")
+
+res.idc.degs <- res.idc.complete[res.idc.complete$padj<0.05 & abs(res.idc.complete$log2FoldChange)>1,]
+summary(res.idc.degs)
+
+## Distribution of the p-adjusted value
+summary(res.idc.degs$padj)
+hist(res.idc.degs$padj, main="Distribution of the adjusted p-value in the DEGs of IDC", xlab="Adjusted p-value")
+################################################################################
+## Repair genes subsetting 
+res.idc.rep <- res.idc[rownames(res.idc) %in% repair.genes$symbol,]
+summary(res.idc.rep)
+## out of 285 genes, 0 up-regulated, 36 down-regulated
+res.idc.degs.rep <- res.idc.degs[rownames(res.idc.degs) %in% repair.genes$symbol,]
+summary(res.idc.degs.rep)
+
+## Distribution of the LFC and p-adjusted value
+summary(res.idc.rep$log2FoldChange)
+hist(res.idc.rep$log2FoldChange, main="Distribution of the Log2 fold change in the results (Repair Genes Only) of IDC", xlab="Log2 Fold Change")
+summary(res.idc.rep$padj)
+hist(res.idc.rep$padj, main="Distribution of the adjusted p-value in the results (Repair Genes Only) of IDC", xlab="Adjusted p-value")
+
+summary(res.idc.degs.rep$padj)
+hist(res.idc.degs.rep$padj, main="Distribution of the adjusted p-value in the Repair DEGs of IDC", xlab="Adjusted p-value")
+################################################################################
+## Plotting: 1. MAplot
+plotMA(res.idc, alpha=0.05, main="MA plot of the not-mormalized DESeq Results of IDC")
+################################################################################
+## Plotting: 2. EnhancedVolcano Plot
+EnhancedVolcano(res.idc, 
+                lab = rownames(res.idc), 
+                x = 'log2FoldChange', 
+                y = 'pvalue',
+                title = "Volcano plot of the not-normalized DESeq results",
+                pCutoff = 0.05,
+                FCcutoff = 1.0,
+                legendPosition = "right")
+################################################################################
+## subset vsd
+dds.vsd.idc <- dds.vsd[,dds.vsd$group %in% c("IDC_Normal", "IDC_Tumor")]
+dds.vsd.idc
+table(dds.vsd.idc$group)
+dds.vsd.idc$group <- factor(dds.vsd.idc$group)
+table(dds.vsd.idc$group)
+
+## DEGs
+dds.vsd.idc.degs <- dds.vsd.idc[rownames(dds.vsd.idc) %in% rownames(res.idc.degs),]
+dds.vsd.idc.degs
+
+## Repair DEGs
+dds.vsd.idc.degs.rep <- dds.vsd.idc.degs[rownames(dds.vsd.idc.degs) %in% repair.genes$symbol ,]
+dds.vsd.idc.degs.rep
+################################################################################
+## Plotting: 3. PCA
+pca.plot.vsd.idc <- plotPCA(dds.vsd.idc, intgroup="group", ntop = 25531, returnData=TRUE)
+percentVar <- round(100 * attr(pca.plot.vsd.idc, "percentVar"))
+ggplot(pca.plot.vsd.idc, aes(PC1, PC2, color=group)) + 
+  geom_point(size=1) + stat_ellipse(type = "norm") + 
+  # xlab(percentage[1]) + ylab(percentage[2]) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) + ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ggtitle("PCA plot of the vst-normalized DESeq DataSet (ntop = 25531)")
+rm(percentVar)
+################################################################################
+## Plotting: 4. Dispersion estimate
+plotDispEsts(dds.run.idc)
+################################################################################
+## Plotting: 7. Boxplot with dots
+source('scripts/plotting_gene.R')
+genes.list <- rownames(res.idc.degs.rep)
+par(mfrow=c(3,3))
+for (gen in genes.list){
+  plotting_gene(dds.run.idc, gen)
+}
+par(mfrow=c(1,1))
+################################################################################
+## Plotting: 8. Heatmap & Dendogram
+colors2 <- colorRampPalette(rev(brewer.pal(9, "RdBu")))(255)
+heatmap.2(assay(dds.vsd.idc.degs.rep), col=colors2,
+          scale="row", trace="none", labCol=substring(colnames(dds.vsd.idc.degs.rep), 6),
+          # dendrogram = "row",
+          Colv=order(dds.vsd.idc.degs.rep$Sample.Type), Rowv=TRUE,
+          ColSideColors = c(Primary.Tumor="darkgreen", Solid.Tissue.Normal="orange")[colData(dds.vsd.idc.degs.rep)$Sample.Type],
+          key =TRUE, key.title="Heatmap Key",
+          main="Heatmap of the top 36 differentially expressed repair genes in IDC")
---
+++ scripts/mixed.R
@@ -0,0 +1,113 @@
+### Mixed
+################################################################################
+## Extracting dds.run.mixed from dds.run
+dds.run.mixed <- dds.run[,dds.run$group %in% c("Mixed_Normal", "Mixed_Tumor")]
+dds.run.mixed
+table(dds.run.mixed$group)
+dds.run.mixed$group <- factor(dds.run.mixed$group)
+table(dds.run.mixed$group)
+################################################################################
+## Creating the results (RES) object from the original dds.run
+res.mixed <- results(dds.run, contrast=c("group", "Mixed_Normal", "Mixed_Tumor"), alpha=0.05,lfcThreshold=1)
+summary(res.mixed)
+
+## Distribution of the LFC and p-adjusted value
+summary(res.mixed$log2FoldChange)
+hist(res.mixed$log2FoldChange, main="Distribution of the Log2 fold change in the results of Mixed", xlab="Log2 Fold Change")
+summary(res.mixed$padj)
+hist(res.mixed$padj, main="Distribution of the adjusted p-value in the results of Mixed", xlab="Adjusted p-value")
+
+## complete cases only
+res.mixed.complete <- res.mixed[complete.cases(res.mixed),]
+################################################################################
+## Converting the results to DF
+res.mixed.df <- as.data.frame(res.mixed)
+res.mixed.df <- res.mixed.df[order(res.mixed.df$padj),]
+write.csv(res.mixed.df, "saved_objects/Mixed.results.sorted.csv")
+################################################################################
+## Extracting DEGs
+res.mixed.df.degs <- res.mixed.df[res.mixed.df$padj<0.05 & abs(res.mixed.df$log2FoldChange)>1,]
+write.csv(res.mixed.df.degs, "saved_objects/mixed-degs-p.05-LFC1.csv")
+
+res.mixed.degs <- res.mixed.complete[res.mixed.complete$padj<0.05 & abs(res.mixed.complete$log2FoldChange)>1,]
+summary(res.mixed.degs)
+
+## Distribution of the p-adjusted value
+summary(res.mixed.degs$padj)
+hist(res.mixed.degs$padj, main="Distribution of the adjusted p-value in the DEGs of Mixed", xlab="Adjusted p-value")
+################################################################################
+## Repair genes subsetting 
+res.mixed.rep <- res.mixed[rownames(res.mixed) %in% repair.genes$symbol,]
+summary(res.mixed.rep)
+## out of 285 genes, 0 up-regulated, 6 down-regulated
+res.mixed.degs.rep <- res.mixed.degs[rownames(res.mixed.degs) %in% repair.genes$symbol,]
+summary(res.mixed.degs.rep)
+
+## Distribution of the LFC and p-adjusted value
+summary(res.mixed.rep$log2FoldChange)
+hist(res.mixed.rep$log2FoldChange, main="Distribution of the Log2 fold change in the results (Repair Genes Only) of Mixed", xlab="Log2 Fold Change")
+summary(res.mixed.rep$padj)
+hist(res.mixed.rep$padj, main="Distribution of the adjusted p-value in the results (Repair Genes Only) of Mixed", xlab="Adjusted p-value")
+
+summary(res.mixed.degs.rep$padj)
+hist(res.mixed.degs.rep$padj, main="Distribution of the adjusted p-value in the Repair DEGs of Mixed", xlab="Adjusted p-value")
+################################################################################
+## Plotting: 1. MAplot
+plotMA(res.mixed, alpha=0.05, main="MA plot of the not-mormalized DESeq Results of Mixed")
+################################################################################
+## Plotting: 2. EnhancedVolcano Plot
+EnhancedVolcano(res.mixed, 
+                lab = rownames(res.mixed), 
+                x = 'log2FoldChange', 
+                y = 'pvalue',
+                title = "Volcano plot of the not-normalized DESeq results",
+                pCutoff = 0.05,
+                FCcutoff = 1.0,
+                legendPosition = "right")
+################################################################################
+## subset vsd
+dds.vsd.mixed <- dds.vsd[,dds.vsd$group %in% c("Mixed_Normal", "Mixed_Tumor")]
+dds.vsd.mixed
+table(dds.vsd.mixed$group)
+dds.vsd.mixed$group <- factor(dds.vsd.mixed$group)
+table(dds.vsd.mixed$group)
+
+## DEGs
+dds.vsd.mixed.degs <- dds.vsd.mixed[rownames(dds.vsd.mixed) %in% rownames(res.mixed.degs),]
+dds.vsd.mixed.degs
+
+## Repair DEGs
+dds.vsd.mixed.degs.rep <- dds.vsd.mixed.degs[rownames(dds.vsd.mixed.degs) %in% repair.genes$symbol ,]
+dds.vsd.mixed.degs.rep
+################################################################################
+## Plotting: 3. PCA
+pca.plot.vsd.mixed <- plotPCA(dds.vsd.mixed, intgroup="group", ntop = 25531, returnData=TRUE)
+percentVar <- round(100 * attr(pca.plot.vsd.mixed, "percentVar"))
+ggplot(pca.plot.vsd.mixed, aes(PC1, PC2, color=group)) + 
+  geom_point(size=1) + stat_ellipse(type = "norm") + 
+  # xlab(percentage[1]) + ylab(percentage[2]) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) + ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ggtitle("PCA plot of the vst-normalized DESeq DataSet (ntop = 25531)")
+rm(percentVar)
+################################################################################
+## Plotting: 4. Dispersion estimate
+plotDispEsts(dds.run.mixed)
+################################################################################
+## Plotting: 7. Boxplot with dots
+source('scripts/plotting_gene.R')
+genes.list <- rownames(res.mixed.degs.rep)
+par(mfrow=c(2,3))
+for (gen in genes.list){
+  plotting_gene(dds.run.mixed, gen)
+}
+par(mfrow=c(1,1))
+################################################################################
+## Plotting: 8. Heatmap & Dendogram
+colors2 <- colorRampPalette(rev(brewer.pal(9, "RdBu")))(255)
+heatmap.2(assay(dds.vsd.mixed.degs.rep), col=colors2,
+          scale="row", trace="none", labCol=substring(colnames(dds.vsd.mixed.degs.rep), 6),
+          # dendrogram = "row",
+          Colv=order(dds.vsd.mixed.degs.rep$Sample.Type), Rowv=TRUE,
+          ColSideColors = c(Primary.Tumor="darkgreen", Solid.Tissue.Normal="orange")[colData(dds.vsd.mixed.degs.rep)$Sample.Type],
+          key =TRUE, key.title="Heatmap Key",
+          main="Heatmap of the top 36 differentially expressed repair genes in Mixed")
