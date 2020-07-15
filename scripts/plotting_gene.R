20987ebb71e1f6c9bd3c2d85073a384016c3c252
ruwaa-mohamed <ruwaa.ibrahem@gmail.com>
2020-07-15 17:26:33 +0200

    generalization of the function

--- scripts/plotting_gene.R
+++ scripts/plotting_gene.R
@@ -1,9 +1,10 @@
-plotting_gene <- function(gene){
-  boxplot(t(assays(dds.run.plot[gene])[["counts"]])~dds.run.plot$Sample.Type, 
+plotting_gene <- function(dds.run, gene){
+  boxplot(t(assays(dds.run[gene])[["counts"]])~dds.run$Sample.Type, 
           range=0, las=1, log='y', boxwex=.4,
-          at=c(0.0, 0.5), cex.lab = 1.5, cex.axis = 1.5,
+          at=c(0.0, 0.5), #cex.lab = 1.5, cex.axis = 1.5,
           xlab=NULL, ylab=NULL,
+          main=gene,
           col=c("darksalmon", "darkred"))
-  stripchart(t(assays(dds.run.plot[gene])[["counts"]])~dds.run.plot$Sample.Type, 
+  stripchart(t(assays(dds.run[gene])[["counts"]])~dds.run$Sample.Type, 
              vertical=TRUE, method='jitter', add=TRUE, pch=23, col="black", cex=1, at=c(0, 0.5)) 
 }
