664e339c9e91c47b990969af7800de594f23e1f8
ruwaa-mohamed <ruwaa.ibrahem@gmail.com>
2020-08-24 21:35:56 +0200

    adding options to the log scale of plotting

--- scripts/plotting_gene.R
+++ scripts/plotting_gene.R
@@ -1,10 +1,11 @@
-plotting_gene <- function(dds.run, gene){
+plotting_gene <- function(dds.run, gene, l='y'){
   boxplot(t(assays(dds.run[gene])[["counts"]])~dds.run$Sample.Type, 
-          range=0, las=1, log='y', boxwex=.4,
+          range=0, las=1, log=l, boxwex=.4,
           at=c(0.0, 0.5), #cex.lab = 1.5, cex.axis = 1.5,
           xlab=NULL, ylab=NULL,
           main=gene,
           col=c("darksalmon", "darkred"))
   stripchart(t(assays(dds.run[gene])[["counts"]])~dds.run$Sample.Type, 
-             vertical=TRUE, method='jitter', add=TRUE, pch=23, col="black", cex=1, at=c(0, 0.5)) 
+             vertical=TRUE, method='jitter', add=TRUE, 
+             pch=20, col="black", cex=0.5, at=c(0, 0.5)) 
 }
