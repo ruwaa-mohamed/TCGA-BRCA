0cd1a073d70162661ba581a54f9450a5c0af8645
ruwaa-mohamed <ruwaa.ibrahem@gmail.com>
2020-09-03 03:18:41 +0200

    minor mods

--- scripts/link.dems.degs.R
+++ scripts/link.dems.degs.R
@@ -2,27 +2,28 @@
 ## Merging DEGs and DEMs by dplyr
 library(dplyr)
 
-hsa_MTI <- read.xlsx("raw_data/miRNA-DBs/hsa_MTI.xlsx")
-substring(hsa_MTI$miRNA, 7) = 'r'
+# hsa_MTI <- read.xlsx("raw_data/miRNA-DBs/hsa_MTI.xlsx")
+# substring(hsa_MTI$miRNA, 7) = 'r'
 
-mir.gene.pairs <- unique(hsa_MTI[, c("miRNA", "Target.Gene")])
-mir.gene.pairs.rep <- mir.gene.pairs[mir.gene.pairs$Target.Gene %in% repair.genes$symbol,]
+mir.gene.pairs <- read.csv("saved_objects/mir.gene.pairs.csv", header=TRUE)
+mir.gene.pairs.rep <- read.csv("saved_objects/mir.gene.pairs.rep.csv", header=TRUE)
+mir.gene.pairs.merged <- read.csv("saved_objects/mir.gene.pairs.merged.csv", header=TRUE)
 ################################################################################
 ## IDC DEGs
-idc.degs.rep <- as.data.frame(res.idc.degs.rep)
+idc.degs.rep <- read.csv("saved_objects/res.idc.degs.rep.csv", header = TRUE)
 idc.degs.rep <- cbind(Target.Gene=rownames(idc.degs.rep), idc.degs.rep)
 idc.degs.rep <- mutate(idc.degs.rep, degs = log2FoldChange > 0)
 idc.degs.rep$degs <- as.factor(idc.degs.rep$degs)
-levels(idc.degs.rep$degs) <- c("up")
+levels(idc.degs.rep$degs) <- c("Gene Up-regulated")
 table(idc.degs.rep$degs)
 idc.degs.rep <- idc.degs.rep[,c("Target.Gene", "degs")]
 ################################################################################
 ## IDC DEMs
-idc.dems <- as.data.frame(mirna.res.idc.dems) 
+idc.dems <- read.csv("saved_objects/mirna.res.idc.dems.csv", header = TRUE)
 idc.dems <- cbind(miRNA=rownames(idc.dems), idc.dems)
 idc.dems <- mutate(idc.dems, dems = log2FoldChange > 0)
 idc.dems$dems <- as.factor(idc.dems$dems)
-levels(idc.dems$dems) <- c("down", "up")
+levels(idc.dems$dems) <- c("miR Down-regulated", "miR Up-regulated")
 table(idc.dems$dems)
 idc.dems <- idc.dems[,c("miRNA", "dems")]
 ################################################################################
@@ -31,6 +32,7 @@ idc.degs.dems <- mir.gene.pairs.rep
 idc.degs.dems <- full_join(idc.degs.dems, idc.dems, by="miRNA")
 idc.degs.dems <- full_join(idc.degs.dems, idc.degs.rep, by="Target.Gene")
 idc.degs.dems <- idc.degs.dems[complete.cases(idc.degs.dems),]
-write.table(idc.degs.dems, "saved_objects/idc.degs.dems.csv", sep=",", quote=FALSE, row.names=FALSE)
+colnames(idc.degs.dems) <- c("miRNA", "Target Gene", "DEMs", "DEGs")
+write.table(idc.degs.dems, "saved_objects/T1-idc.degs.dems.csv", sep=",", quote=FALSE, row.names=FALSE)
 ################################################################################
 ## There's no IDCLC DEGs Repair!
--- scripts/plotting_gene.R
+++ scripts/plotting_gene.R
@@ -1,11 +1,11 @@
-plotting_gene <- function(dds.run, gene, l='y'){
-  boxplot(t(assays(dds.run[gene])[["counts"]])~dds.run$Sample.Type, 
+plotting_gene <- function(dds.run, gene, l='y', atr="counts"){
+  boxplot(t(assays(dds.run[gene])[[atr]])~dds.run$Sample.Type, 
           range=0, las=1, log=l, boxwex=.4,
           at=c(0.0, 0.5), #cex.lab = 1.5, cex.axis = 1.5,
           xlab=NULL, ylab=NULL,
           main=gene,
           col=c("darksalmon", "darkred"))
-  stripchart(t(assays(dds.run[gene])[["counts"]])~dds.run$Sample.Type, 
+  stripchart(t(assays(dds.run[gene])[[atr]])~dds.run$Sample.Type, 
              vertical=TRUE, method='jitter', add=TRUE, 
              pch=20, col="black", cex=0.5, at=c(0, 0.5)) 
 }
